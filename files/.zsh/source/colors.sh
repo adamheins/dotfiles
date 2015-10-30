23370b8c785e827b0dde0388b41e4d107976c9d5
Adam Heins <mail@adamheins.com>
2015-10-30 00:01:55 -0700

    Update dotfiles.

---
+++ files/.zsh/source/colors.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Print all of the terminal colors.
+colors() {
+  local count=0
+  local cols=$(tput cols)
+  for i in {0..255}; do
+    ((count+=4))
+    if [ $count -gt $cols ]; then
+      printf "\n"
+      count=4
+    fi
+    printf "\x1b[38;5;${i}m%03s " "${i}"
+  done
+  printf "\n"
+}
