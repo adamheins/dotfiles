e4be91e18994a356b1edea5402bea62f97846e68
Adam Heins <mail@adamheins.com>
2017-01-12 12:37:14 -0500

    Local scripts can now also be symlinks.
    This is not supported for common scripts since they should not be
    symlinks to elsewhere on the local system; they should be packaged in
    this repository.

--- files/.zsh/local.sh
+++ files/.zsh/local.sh
@@ -7,7 +7,12 @@ if [ -d $local/comp ]; then
 fi
 
 # Scripts to be sourced.
+# Note the glob pattern:
+#   .  Match plain files
+#   ,  "OR"
+#   @  Match symbolic files
+#   N  NULL_GLOB - don't print an error if nothing matched
 if [ -d $local/scripts ]; then
-  for f in $local/scripts/*.sh(.N); do source "$f"; done
-  for f in $local/scripts/*.zsh(.N); do source "$f"; done
+  for f in $local/scripts/*.sh(.,@N); do source "$f"; done
+  for f in $local/scripts/*.zsh(.,@N); do source "$f"; done
 fi
