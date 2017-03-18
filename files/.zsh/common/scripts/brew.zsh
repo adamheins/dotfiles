2cf578a1deb9f2d014eb4572fa25e164860cdd2e
Adam Heins <mail@adamheins.com>
2017-03-18 17:33:05 -0400

    Add script for linuxbrew paths.

---
+++ files/.zsh/common/scripts/brew.zsh
@@ -0,0 +1,5 @@
+if [ command -v brew >/dev/null 2>&1 ]; then
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+fi
