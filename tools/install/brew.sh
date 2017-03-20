96fb0f190a6885b90961c865d4536395d62565c9
Adam Heins <mail@adamheins.com>
2017-03-19 22:53:35 -0400

    Properly check if brew already installed.

--- tools/install/brew.sh
+++ tools/install/brew.sh
@@ -2,8 +2,13 @@
 
 # Install brew package manager.
 
-if [ $(uname) = "Darwin" ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if command -v brew >/dev/null 2>&1; then
+  echo "brew already installed."
 else
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  if [ $(uname) = "Darwin" ]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
 fi
+
