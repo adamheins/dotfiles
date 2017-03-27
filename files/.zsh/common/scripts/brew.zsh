9ba44721ba17ff330b006c6da33b2b85e9b42d07
Adam Heins <mail@adamheins.com>
2017-03-27 14:38:41 -0400

    Don't check for linuxbrew on Mac.

--- files/.zsh/common/scripts/brew.zsh
+++ files/.zsh/common/scripts/brew.zsh
@@ -1,4 +1,5 @@
-if [ -d ~/.linuxbrew ]; then
+# If we've got linuxbrew installed, set it up.
+if [ ! $(uname) = "Darwin" ] && [ -d ~/.linuxbrew ]; then
   export PATH="$HOME/.linuxbrew/bin:$PATH"
   export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
   export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
