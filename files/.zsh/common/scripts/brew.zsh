0641b0821074beba6a3afd9a7a8f681a37301199
Adam Heins <mail@adamheins.com>
2017-04-17 12:06:51 -0400

    Add extra brew paths.

--- files/.zsh/common/scripts/brew.zsh
+++ files/.zsh/common/scripts/brew.zsh
@@ -1,6 +1,8 @@
 # If we've got linuxbrew installed, set it up.
 if [ ! $(uname) = "Darwin" ] && [ -d ~/.linuxbrew ]; then
   export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export PATH="$HOME/.linuxbrew/sbin:$PATH"
   export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
   export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+  export XDG_DATA_DIRS="/home/adam/.linuxbrew/share:$XDG_DATA_DIRS"
 fi
