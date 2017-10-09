472c7161a91b46c83ddc9fb7a6d9ecb9653618b4
Adam Heins <mail@adamheins.com>
2017-10-09 17:12:37 -0400

    Check that brew isn't on the PATH before adding it.

--- files/.zsh/common/scripts/brew.zsh
+++ files/.zsh/common/scripts/brew.zsh
@@ -1,8 +1,13 @@
 # If we've got linuxbrew installed, set it up.
 if [ ! $(uname) = "Darwin" ] && [ -d ~/.linuxbrew ]; then
-  export PATH="$HOME/.linuxbrew/bin:$PATH"
-  export PATH="$HOME/.linuxbrew/sbin:$PATH"
-  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
-  export XDG_DATA_DIRS="/home/adam/.linuxbrew/share:$XDG_DATA_DIRS"
+  case ":$PATH:" in
+    *:$HOME/.linuxbrew/bin:*) ;;
+    *)
+      export PATH="$HOME/.linuxbrew/bin:$PATH"
+      export PATH="$HOME/.linuxbrew/sbin:$PATH"
+      export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+      export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+      export XDG_DATA_DIRS="/home/adam/.linuxbrew/share:$XDG_DATA_DIRS"
+    ;;
+  esac
 fi
