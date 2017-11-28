de1509dd854aa4d616bcb0acede9e774a2b0b444
Adam Heins <mail@adamheins.com>
2017-11-27 21:42:20 -0500

    Add brew completion path.

--- files/.zsh/common/scripts/brew.zsh
+++ files/.zsh/common/scripts/brew.zsh
@@ -1,6 +1,7 @@
 # If we've got linuxbrew installed, set it up.
 if [ ! $(uname) = "Darwin" ] && [ -d ~/.linuxbrew ]; then
   path=($HOME/.linuxbrew/bin $HOME/.linuxbrew/sbin $path)
+  fpath=(/home/linuxbrew/.linuxbrew/share/zsh-completions $fpath)
   manpath=($HOME/.linuxbrew/share/man $manpath)
   infopath=($HOME/.linuxbrew/share/info $infopath)
   xdg_data_dirs=($HOME/.linuxbrew/share $xdg_data_dirs)
