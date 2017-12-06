426f6302d7b98cf0f3d975070f61edf8cf69bc41
Adam Heins <mail@adamheins.com>
2017-12-06 17:32:50 -0500

    Remove Darwin check.
    There is no point checking if we're on Darwin (i.e. macOS), since the
    the install directory would be named differently.

--- files/.zsh/common/scripts/brew.zsh
+++ files/.zsh/common/scripts/brew.zsh
@@ -1,11 +1,14 @@
 # If we've got brew installed, set it up.
-brewpath=~/.linuxbrew
 
-if [ ! $(uname) = "Darwin" ] && [ -d $brewpath ]; then
-  path=($brewpath/bin $brewpath/sbin $path)
-  fpath=($brewpath/share/zsh-completions $fpath)
-  manpath=($brewpath/share/man $manpath)
-  infopath=($brewpath/share/info $infopath)
-  xdg_data_dirs=($brewpath/share $xdg_data_dirs)
-  pythonpath=($brewpath/lib/python2.7/site-packages $pythonpath)
+# The expectation is that brew is either installed at ~/.linuxbrew or
+# ~/.linuxbrew is symlinked to the actual install location.
+BREW_PATH=~/.linuxbrew
+
+if [ -d $BREW_PATH ]; then
+  path=($BREW_PATH/bin $BREW_PATH/sbin $path)
+  fpath=($BREW_PATH/share/zsh-completions $fpath)
+  manpath=($BREW_PATH/share/man $manpath)
+  infopath=($BREW_PATH/share/info $infopath)
+  xdg_data_dirs=($BREW_PATH/share $xdg_data_dirs)
+  pythonpath=($BREW_PATH/lib/python2.7/site-packages $pythonpath)
 fi
