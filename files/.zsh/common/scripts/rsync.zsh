83f6b98b79905794e3ec25610f84a0320667b95b
Adam Heins <mail@adamheins.com>
2018-02-06 13:44:30 -0500

    Fix rpull behaviour.

--- files/.zsh/common/scripts/rsync.zsh
+++ files/.zsh/common/scripts/rsync.zsh
@@ -1,5 +1,6 @@
 # Convenience functions for rsyncing.
 
+# Push files to the destination.
 rpush() {
   if [ -f .rsync ]; then
     config=$(cat .rsync)
@@ -13,21 +14,24 @@ rpush() {
     fi
   else
     echo "No .rsync file found."
+    return 1
   fi
 }
 
+# Pull files from the destination.
 rpull() {
   if [ -f .rsync ]; then
     config=$(cat .rsync)
     dest=$(echo $config | head -n 1)
     opts=($(echo $config | tail -n 1))
     if [[ -n $1 && $1 = "--wet" ]]; then
-      rsync $opts "$dest" .
+      rsync $opts "$dest" ..
     else
-      rsync $opts --dry-run "$dest" .
+      rsync $opts --dry-run "$dest" ..
       echo "Dry run. Run again with --wet for a real sync."
     fi
   else
     echo "No .rsync file found."
+    return 1
   fi
 }
