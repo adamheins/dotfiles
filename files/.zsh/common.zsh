1b1c211fee4da2447e3edb3ba8d0708edd1f428a
Adam Heins <mail@adamheins.com>
2018-02-10 12:17:12 -0500

    Add utility executables.

--- files/.zsh/common.zsh
+++ files/.zsh/common.zsh
@@ -1,22 +1,30 @@
 # Directories.
 here=${0:a:h}
-common=$here/common
-scripts=$common/scripts
+common_dir=$here/common
+scripts_dir=$common_dir/scripts
+comp_dir=$common_dir/comp
+bin_dir=$common_dir/bin
 
 # Scripts to be sourced.
-if [ -d $scripts ]; then
+if [ -d $scripts_dir ]; then
 
   # Scripts to be sourced before the others. Should not depend on each other.
-  if [ -d $scripts/before ]; then
-    for f in $scripts/before/*.sh(.N); do source "$f"; done
-    for f in $scripts/before/*.zsh(.N); do source "$f"; done
+  if [ -d $scripts_dir/before ]; then
+    for f in $scripts_dir/before/*.sh(.N); do source "$f"; done
+    for f in $scripts_dir/before/*.zsh(.N); do source "$f"; done
   fi
 
-  for f in $scripts/*.sh(.N); do source "$f"; done
-  for f in $scripts/*.zsh(.N); do source "$f"; done
+  # Other scripts.
+  for f in $scripts_dir/*.sh(.N); do source "$f"; done
+  for f in $scripts_dir/*.zsh(.N); do source "$f"; done
 fi
 
 # Completions.
-if [ -d $common/comp ]; then
-  fpath=($common/comp $path)
+if [ -d $comp_dir ]; then
+  fpath=($comp_dir $path)
+fi
+
+# Executables.
+if [ -d $bin_dir ]; then
+  path=($bin_dir $path)
 fi
