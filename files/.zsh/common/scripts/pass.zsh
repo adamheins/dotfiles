68963c273eb7e98b5310062f344e6872eff616e0
Adam Heins <mail@adamheins.com>
2018-02-11 21:51:31 -0500

    Use bash completion for gopass.

--- files/.zsh/common/scripts/pass.zsh
+++ files/.zsh/common/scripts/pass.zsh
@@ -1,5 +1,10 @@
 if onpath gopass; then
-  source <(gopass completion zsh)
+  # Use the bash completion for gopass because the zsh completion is somewhat
+  # broken.
+  autoload -U compinit && compinit
+  autoload -U bashcompinit && bashcompinit
+  source <(gopass completion bash)
+
   alias pass=gopass
   alias p="gopass show -c"
 elif onpath pass; then
