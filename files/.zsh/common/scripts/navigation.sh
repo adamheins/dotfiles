abbba0e75b6e94addbb8a8401bd8d9405c041ab2
Adam Heins <mail@adamheins.com>
2018-08-20 17:04:25 -0400

    Improve dir navigation aliases.

--- files/.zsh/common/scripts/navigation.sh
+++ files/.zsh/common/scripts/navigation.sh
@@ -4,14 +4,15 @@
 alias dirs="dirs -v"
 
 # Switch back to previous directories.
-alias -- -="cd +1 >/dev/null 2>&1"
-alias -- --="cd +2 >/dev/null 2>&1"
-alias -- ---="cd +3 >/dev/null 2>&1"
+alias -- -1="cd +1 >/dev/null 2>&1"
+alias -- -2="cd +2 >/dev/null 2>&1 || -1"
+alias -- -3="cd +3 >/dev/null 2>&1 || -2"
+alias -- -4="cd +4 >/dev/null 2>&1 || -3"
+alias -- -5="cd +5 >/dev/null 2>&1 || -4"
 
-alias -- -2="cd +2 >/dev/null 2>&1"
-alias -- -3="cd +3 >/dev/null 2>&1"
-alias -- -4="cd +4 >/dev/null 2>&1"
-alias -- -5="cd +5 >/dev/null 2>&1"
+alias -- -="-1"
+alias -- --="-2"
+alias -- ---="-3"
 
 # Navigate to parent directories.
 alias ..="cd .."
