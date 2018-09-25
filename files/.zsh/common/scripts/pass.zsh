5dd0e5c9cf04280cadbd1fb52514455a2e925886
Adam Heins <mail@adamheins.com>
2018-09-25 08:25:31 -0400

    Add GOPATH to PATH.

--- files/.zsh/common/scripts/pass.zsh
+++ files/.zsh/common/scripts/pass.zsh
@@ -1,10 +1,4 @@
 if onpath gopass; then
-  # Use the bash completion for gopass because the zsh completion is somewhat
-  # broken.
-  autoload -U compinit && compinit
-  autoload -U bashcompinit && bashcompinit
-  source <(gopass completion bash)
-
   alias pass=gopass
   alias p="gopass show -c"
 elif onpath pass; then
