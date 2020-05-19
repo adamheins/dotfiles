48a52a74c5b974bba2bf0c8525707f81a243f2db
Adam Heins <mail@adamheins.com>
2020-05-19 12:08:40 -0400

    Improve dotfiler script.

--- dotfiler.zsh
+++ dotfiler.zsh
@@ -1,21 +1,37 @@
 #!/bin/zsh
 
-scriptpath=${0:a:h}
+dotfiles=${0:a:h}
+
+
+# Create a symlink at location $1 with source $2. Backup $1 if it already
+# exists.
+backup-and-link() {
+  [ -z "$2" ] && return 1
+
+  if [ -e "$1" ]; then
+    echo "$1" exists; moving to "$1".back
+    mv "$1" "$1".back
+  fi
+
+  ln -s "$2" "$1"
+  echo Created symlink "$1 -> $2"
+}
 
 # Link dotfiles.
 
-ln -s "$scriptpath"/files/.zsh ~/.zsh
-ln -s "$scriptpath"/files/.zshrc ~/.zshrc
-ln -s "$scriptpath"/files/.gitignore ~/.gitignore
-ln -s "$scriptpath"/files/.tmux.conf ~/.tmux.conf
-ln -s "$scriptpath"/files/.editrc ~/.editrc
-ln -s "$scriptpath"/files/.inputrc ~/.inputrc
-
-echo Created symlinks.
+backup-and-link ~/.zsh       "$dotfiles"/files/.zsh
+backup-and-link ~/.zshrc     "$dotfiles"/files/.zshrc
+backup-and-link ~/.gitignore "$dotfiles"/files/.gitignore
+backup-and-link ~/.tmux.conf "$dotfiles"/files/.tmux.conf
+backup-and-link ~/.editrc    "$dotfiles"/files/.editrc
+backup-and-link ~/.inputrc   "$dotfiles"/files/.inputrc
 
 # Generate the global git config file.
 
-[ -f ~/.gitconfig ] && rm ~/.gitconfig
+if [ -f ~/.gitconfig ]; then
+  echo ~/.gitconfig exists; moving to ~/.gitconfig.back
+  mv ~/.gitconfig ~/.gitconfig.back
+fi
 
 git config --global push.default "simple"
 git config --global user.name "Adam Heins"
