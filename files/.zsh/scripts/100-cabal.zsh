e5aeb6de1170e0694fd59e53d43387ecd583ba5d
Adam Heins <mail@adamheins.com>
2020-12-01 20:09:38 -0500

    Remove unused scripts.

--- files/.zsh/scripts/100-cabal.zsh
+++
@@ -1,5 +0,0 @@
-CABAL_PATH="$HOME/.cabal"
-
-if [ -d $CABAL_PATH ]; then
-  path=($CABAL_PATH/bin $path)
-fi
--- files/.zsh/scripts/100-cargo.zsh
+++
@@ -1,7 +0,0 @@
-# Rust package manager
-#
-CARGO_PATH="$HOME/.cargo"
-
-if [ -d $CARGO_PATH ]; then
-  path=($CARGO_PATH/bin $path)
-fi
