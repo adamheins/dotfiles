c354785ba7a03e737fc66beb1ac761c66dc3263d
Adam Heins <mail@adamheins.com>
2020-12-01 20:27:43 -0500

    Remove user-specific path.

--- files/.zsh/scripts/100-nix.sh
+++ files/.zsh/scripts/100-nix.sh
@@ -1,4 +1,4 @@
 # Nix package manager.
-if [ -e /home/adam/.nix-profile/etc/profile.d/nix.sh ]; then
-  . /home/adam/.nix-profile/etc/profile.d/nix.sh
+if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+  . ~/.nix-profile/etc/profile.d/nix.sh
 fi
